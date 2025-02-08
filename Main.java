1bd408db1709f98ac9a8608099eb43634e1c2efc
MrIfu <hripunovila371@gmail.com>
2025-02-08 20:02:52 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args){
+// TODO продвинутся в изучении...
+        System.out.println("Hello world,Hello java,hello ifu!");
+
+        }
+
+    }
+
+
+
